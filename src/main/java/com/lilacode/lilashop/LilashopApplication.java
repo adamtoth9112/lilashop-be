fdba72b2e5f61a332c6bf05727ddcdda8d31d118
Adam Toth <adamtoth9112@gmail.com>
2024-11-14 21:57:55 +0100

    init

---
+++ src/main/java/com/lilacode/lilashop/LilashopApplication.java
@@ -0,0 +1,13 @@
+package com.lilacode.lilashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LilashopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LilashopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lilacode/lilashop/LilashopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lilacode.lilashop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LilashopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
